de0733a3f84bb8035d2e2f59dcfc84ac7d94aba8
sarthikgupta <sarthi.abhi@gmail.com>
2026-04-11 15:54:57 +0530

    fix: whatsapp integration

--- services/whatsapp.js
+++ services/whatsapp.js
@@ -83,7 +83,7 @@ async function sendMedicineReminder({ patient, doctorName, clinicPhone, clinicId
 
 async function sendAppointmentReminder({ patient, appointmentDate, appointmentTime, doctorName, clinicAddress, clinicPhone, clinicId }) {
   return sendWhatsAppTemplate(patient.phone, TEMPLATES.appointment_reminder.name,
-    [patient.name, appointmentDate, appointmentTime, doctorName, clinicAddress || '', clinicPhone || '',], clinicId);
+    [patient.name, appointmentDate, appointmentTime, doctorName, clinicAddress, clinicPhone], clinicId);
 }
 
 async function sendLabReminder({ patient, doctorName, clinicName, clinicId }) {
